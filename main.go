7c1b41754160036ec75af1548ec6010d4e5f0ab6
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2020-11-07 00:43:13 +0500

    Updated  project version

--- main.go
+++ main.go
@@ -7,21 +7,33 @@ import (
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
+var bot, _ = tb.NewBot(tb.Settings{
+	// here token
+	Token:  "1446049605:AAFU3Tzrp3SciOBQBS4dROnQFlsntxgy5to", // this token is not real
+	Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+})
+
+// log.Fatal(err)
+
 func main() {
-	b, err := tb.NewBot(tb.Settings{
-		// here token
-		Token:  "1446049605:AAFU3Tzrp3SciOBQBS4dROnQFlsntxgy5to", // this token is not real
-		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
-	})
 
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+	bot.Handle("/start", startHandle)
+	bot.Handle(tb.OnText, textHandle)
+	log.Printf(
+		"Bot connected! FirstName: %s, UserName: @%s, is bot: %t, Bot id: %v.\n",
+		bot.Me.FirstName,
+		bot.Me.Username,
+		bot.Me.IsBot,
+		bot.Me.ID)
+	bot.Start()
+}
 
-	b.Handle("/hello", func(m *tb.Message) {
-		b.Send(m.Sender, "Hello World!")
-	})
+func startHandle(m *tb.Message) {
+	log.Println("Hello", m.Chat.Username)
+	bot.Send(m.Sender, "Hello")
+}
 
-	b.Start()
+func textHandle(m *tb.Message) {
+	// all the text messages that weren't
+	// captured by existing handlers
 }
